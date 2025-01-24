1804f4c2f68a223b2217d924a41fe6801e2c7428
setareh <setarehmousaviifar@gmail.com>
2025-01-24 09:59:54 +0330

    login design and reset password

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.views import LogoutView
+from django.contrib.auth import views as auth_views
 from django.urls import path
 from . import views
 
@@ -42,4 +43,10 @@ urlpatterns = [
     path('register/', views.register_user, name='register'),  
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),  
     path('login/', views.custom_login_view, name='login'), 
+
+    # Reset Password
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='accounts/password_reset.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='accounts/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),
 ]
